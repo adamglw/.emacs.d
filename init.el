f5c1b167b483c1fd4a16633a5e70e78439e5bfb0
adamglw <53101365+adamglw@users.noreply.github.com>
2021-04-17 00:13:05 +0100

    Add evil keybindings

--- init.el
+++ init.el
@@ -46,6 +46,9 @@
 :config
 (ivy-mode 1))
 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 ;; Basic UI config
 (setq inhibit-startup-message t)
 
@@ -60,9 +63,6 @@
 
 ;; Set font and size
 (set-face-attribute 'default nil :font "Hack Nerd Font" :height 105)
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
 ;; Column and line numbers
 (column-number-mode)
@@ -125,3 +125,38 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, itaics are universally disabled
   (load-theme 'doom-gruvbox t))
+
+;; general.el for defining keybindings
+(use-package general)
+
+(defun voodoo/evil-hook ()
+  (dolist (mode '(custom-mode
+                  eshell-mode
+                  git-rebase-mode
+                  erc-mode
+                  circe-server-mode
+                  circe-chat-mode
+                  circe-query-mode
+                  sauron-mode
+                  term-mode))
+    (add-to-list 'evil-emacs-state-modes mode)))
+
+;; Evil mode
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :hook (evil-mode . voodoo/evil-hook)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" evil-next-visual-line)
+  (evil-global-set-key 'motion "k" evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
