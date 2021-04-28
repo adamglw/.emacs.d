5af01e5995672c34c736c4bb2a226da9b20a4a61
adamglw <53101365+adamglw@users.noreply.github.com>
2021-04-28 23:38:49 +0100

    Change theme to doom-palenight

--- init.el
+++ init.el
@@ -130,8 +130,8 @@
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, itaics are universally disabled
-  (load-theme 'doom-gruvbox t)
-;  (load-theme 'doom-palenight t)
+;  (load-theme 'doom-gruvbox t)
+  (load-theme 'doom-palenight t)
 )
 
 ;; general.el for defining keybindings
