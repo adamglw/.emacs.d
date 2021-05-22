7c7a9aa82fe72bbed55f6f4c63db21c0087dee3d
adamglw <53101365+adamglw@users.noreply.github.com>
2021-05-22 23:57:59 +0100

    Customise org headings

--- init.el
+++ init.el
@@ -236,3 +236,13 @@
 (use-package org-bullets
   :after org
   :hook (org-mode . org-bullets-mode))
+
+(dolist (face '((org-level-1 . 1.2)
+		(org-level-2 . 1.1)
+		(org-level-3 . 1.05)
+		(org-level-4 . 1.0)
+		(org-level-5 . 1.1)
+		(org-level-6 . 1.1)
+		(org-level-7 . 1.1)
+		(org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil :font "Noto Sans" :weight 'regular :height (cdr face)))
